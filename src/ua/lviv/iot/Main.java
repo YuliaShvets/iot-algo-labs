8212358443f50473169f9d17bd49c8720c40a466
Yulia Shvets <yuliia-mariia.shvets.ir.2021@lpnu.ua>
2022-09-29 23:41:32 +0300

    added solution for task

--- src/ua/lviv/iot/Main.java
+++ src/ua/lviv/iot/Main.java
@@ -2,6 +2,21 @@ package ua.lviv.iot;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Field field = new Field();
+        int[][] matrix = {
+                {0, 1, 1, 1, 0, 1, 1, 1, 1, 1},
+                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                {1, 1, 1, 1, 1, 1, 1, 1, 0, 1},
+                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                {1, 1, 1, 1, 1, 0, 1, 1, 1, 1},
+                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                {1, 0, 1, 1, 1, 1, 1, 1, 1, 1},
+                {1, 1, 1, 1, 1, 1, 1, 1, 1, 0},
+                {1, 1, 1, 1, 1, 0, 1, 1, 1, 1},
+                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
+        };
+
+        System.out.println(field.findShortPath(matrix));
     }
 }
--- src/ua/lviv/iot/Node.java
+++ src/ua/lviv/iot/Node.java
@@ -4,4 +4,25 @@ public class Node {
     private int dist;
     private int x;
     private int y;
+
+    public Node(int x, int y, int dist) {
+        this.x = x;
+        this.y = y;
+        this.dist = dist;
+    }
+
+    public Node() {
+    }
+
+    public int getDist() {
+        return dist;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
 }
